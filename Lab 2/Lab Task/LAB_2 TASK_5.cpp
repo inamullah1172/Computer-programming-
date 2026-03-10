144677f02cff7b93e291181dc6ae711e69956432
inamullah1172 <inamullha924@gmail.com>
2026-03-10 00:57:04 -0700

    Add files via upload

---
+++ Lab 2/Lab Task/LAB_2 TASK_5.cpp
@@ -0,0 +1,24 @@
+// Name: Syed Inamullah Shah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0758
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+    cout << "Syed Inamullah" << endl;
+
+    return 0;  // program ends
+}
