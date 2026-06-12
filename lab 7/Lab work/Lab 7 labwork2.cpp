b63c509289496eaa9436473e6f13ecb0f1dffe1c
inamullah1172 <inamullha924@gmail.com>
2026-06-12 21:11:57 +0500

    Add files via upload

---
+++ lab 7/Lab work/Lab 7 labwork2.cpp
@@ -0,0 +1,23 @@
+// Name: Syed inamullah shah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0758
+// Section: A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // character array "message" declare kiya
+    // isme aik word store kiya gaya hai
+    char message[] = "HELLO";
+
+    // loop use karke har character ko access karenge
+    // '\0' ka matlab string ka end hota hai
+    for(int i = 0; message[i] != '\0'; i++)
+    {
+        // har character ko alag line me print kar rahe hain
+        cout << message[i] << endl;
+    }
+
+    return 0; // program ka successful end
+}
